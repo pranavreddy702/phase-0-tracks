bd4a16d1049241463a23523463c92022576165b2
pranavreddy702 <pranavreddy76@yahoo.co.in>
2016-07-25 04:20:12 -0400

    nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,29 @@
+
+college_campus = {
+	room1: {
+		student_name: 'Patient 1',
+		room_items: {
+			bed: 1,
+			lamp: 1,
+			chair: 1,
+			desk: 1,
+			bathroom: 1
+		},
+		
+	},
+	room2: {
+		student_name: 'student 2',
+		room_equipment2: {
+			bed: 1,
+			lamp: 1,
+			chair: 1,
+			desk: 1,
+			bathroom: 1
+			},
+		extra_items_needed2: [
+			"towels",
+			"soap",
+			"notebook"
+		]
+	}, 
+	}
