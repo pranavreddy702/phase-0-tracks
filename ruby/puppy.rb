5b76934ee13304f3918bd7eb6d4e3b5ff904e5ed
pranavreddy702 <pranavreddy76@yahoo.co.in>
2016-08-22 11:42:19 -0400

    puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Puppy Class
+
+# CHARACTERISTICS 
+# Playful: yes
+# Loyal: yes
+# Color: varies
+# Size: varies
+# Temperament: varies
+# Eye count: 2
+# Leg count: 4
+# Tail count: 1
+
+# BEHAVIOR
+# Bark
+# Lick
+# Tail-wag
+# Snuggle
+# Explore
